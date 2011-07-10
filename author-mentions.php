7718e4a9934947e85411b3ff8957253d8781cf34
benjaminniess <benjaminniess@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-10 16:03:58 +0000

    commit the first release on the trunk
    git-svn-id: https://plugins.svn.wordpress.org/author-mentions/trunk@407901 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ author-mentions.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Plugin Name: Author mentions
+Description: Replace each `@wordpress user_login@` by the user posts page link in your posts contents.
+Version: 1.0
+Author: Benjamin Niess
+Author URI: http://www.benjamin-niess.fr
+
+---
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+---
+*/
+
+if ( !is_admin() )
+	add_filter( 'the_content', 'autor_mentions_function' );
+
+/**
+ * Replace @authors inside the_content
+ * 
+ * @param string $the_content the post content
+ * @return string the content with new permalink on mentions
+ * @author Benjamin Niess
+ */
+function autor_mentions_function( $the_content ) {
+	return preg_replace_callback( '/@[a-zA-Z]*@/', 'content_callback', $the_content );
+}
+
+/*
+ * Callback function that filter potential authors
+ * 
+ * @param string $mention the mention with the @
+ * @return string the mention with the permalink | the string without any change
+ * @author Benjamin Niess
+ */
+function content_callback( $mention ) {
+	$split_a = str_replace( '@', '', $mention );
+	if ( !isset( $split_a[0] ) || empty( $split_a[0] ) )
+		return $mention;
+	
+	// Check if the user exists
+	$user = get_userdatabylogin( $split_a[0] );
+	if ( empty( $user ) || !isset( $user->user_login ) || empty( $user->user_login ) ){
+		return $mention[0];
+	}
+	
+	// Return the correct link without @@ and with the user display name
+	return '<a href="' . esc_url( get_author_posts_url( $user->ID, $user->user_nicename ) ) . '" class="author_mentions" rel="friend">' . esc_html($user->display_name) . '</a>';
+}
+?>
